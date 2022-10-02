f14b97df67a1debd5c86ac7e842076dc963e2a10
bishaldn <bdn0298@gmail.com>
2022-10-02 17:55:01 +0530

    adding dropdwon list

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 const mobile_nav = document.querySelector('.mobile-nav-btn');
 const show = document.querySelector('.nav');
+const hideImage = document.querySelector('.imageContainer');
 mobile_nav.addEventListener('click', () => toggleNavbar());
 function toggleNavbar() {
     show.classList.toggle("show");
+    hideImage.classList.toggle("hide");
+
 }
+
+
